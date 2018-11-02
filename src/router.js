24646c1d0a9eb303bf7a0783ae8d6593b30e68df
He Wang <he@townsquare.agency>
2018-11-02 16:57:18 +1100

    add markup for date picker

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import Admin from "./views/Admin.vue";
 
 Vue.use(Router);
 
@@ -20,8 +19,15 @@ export default new Router({
 			// route level code-splitting
 			// this generates a separate chunk (about.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
-			// component: () => import(/* webpackChunkName: "about" */ "./views/Admin.vue")
-			component: Admin
+			component: () => import(/* webpackChunkName: "about" */ "./views/Admin.vue")
+		},
+		{
+			path: "/schedule",
+			name: "schedule",
+			// route level code-splitting
+			// this generates a separate chunk (about.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: () => import(/* webpackChunkName: "about" */ "./views/Schedule.vue")
 		}
 	]
 });
---
+++ src/views/Schedule.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <v-date-picker v-model="picker" :allowed-dates="allowedDates" locale="en-GB" full-width :min="current" :landscape="landscape" :reactive="reactive" @click.native="onClick">
+        </v-date-picker>
+        {{this.picker}}
+
+    </div>
+</template>
+
+
+<script>
+export default {
+	data() {
+		return {
+			picker: new Date().toISOString().split("T")[0],
+			landscape: true,
+			reactive: true,
+			current: new Date().toISOString().split("T")[0],
+			pickerDate: null
+		};
+	},
+	methods: {
+		onClick(e) {
+			// e.preventDefault();
+			// e.stopPropagation();
+			console.log(this.picker);
+		},
+		allowedDates: val => {
+			return new Date(val).getDay() !== 0 && new Date(val).getDay() !== 6;
+		}
+	}
+};
+</script>
